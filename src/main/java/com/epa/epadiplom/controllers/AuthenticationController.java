252a3424ece64aa076a8f08f45163f305e901e1c
AusaSwart <october.monroe@mail.ru>
2023-02-24 11:25:53 +0300

    AuthController

--- src/main/java/com/epa/epadiplom/controllers/AuthenticationController.java
+++ src/main/java/com/epa/epadiplom/controllers/AuthenticationController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
--- src/main/java/com/epa/epadiplom/models/service/LoginService.java
+++ src/main/java/com/epa/epadiplom/models/service/LoginService.java
@@ -1,10 +1,27 @@
 package com.epa.epadiplom.models.service;
 
+import com.epa.epadiplom.models.entities.Login;
+import com.epa.epadiplom.models.repositories.LoginRepo;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class LoginService {
+public class LoginService implements UserDetailsService {
     // Here's another one layer of abstraction nd control
     // Methods
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+    private final LoginRepo repository;
+    public LoginService(LoginRepo repository) {
+        this.repository = repository;
+    }
+
+    public Login getByLogin(String login) {
+        return this.repository.getByLogin(login);
+    }
 
 }
